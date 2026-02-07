89e60aabf822e278cf9f681b6e2be9208126a43b
Kim Sohee <blackteatime4@naver.com>
2026-02-07 21:30:32 +0900

    feat : 클라이언트 로그인 페이지 화면 구성

---
+++ NateOnMessengerApp/Client/Models/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace client.Models
+{
+    public class CharMessage
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+        public bool IsMyMessage { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# NateOnMessenger
+C#을 위한 네이트온 클론 프로젝트
